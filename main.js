b09b5fbcbcc3b78ff9c8a05fe03f5ef8d5dda495
Trelel129 <ferrynur77@gmail.com>
2022-09-07 21:18:36 +0700

    update js

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
-function main(){
-    var kanvas = document.getElementById("kanvas");
-    var gl = kanvas.getContext("webgl");
+function main() {
+  // Ambil canvas
+  const canvas = document.getElementById("myCanvas");
+  // Ambil konteks WebGL
+  const gl = canvas.getContext("webgl");
 
-    //vertex shader
-    var vertexShaderCode = `
-    "void main(){
-        
+  const vertexShaderCode = `
+    void main() {
+      gl_Position = vec4(0.3, 0.5, 0.0, 1.0);
+      gl_PointSize = 10.0;
     }
-    `;
-    var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShaderObject, vertexShaderCode);
-    gl.compileShader(vertexShaderObject); //sampai sini sudah jadi .o
+  `;
 
-    //fragment shader
-    var fragmentShaderCode =`
-    void main(){
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexShaderCode);
+  gl.compileShader(vertexShader);
 
-    }`;
-    var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
-    gl.compileShader(fragmentShaderObject); //sampai sini sudah jadi .o
+  const fragmentShaderCode = `
+    void main() {
+      gl_FragColor = vec4(0.5, 0.0, 0.0, 1.0);
+    }
+  `;
 
-    var shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShaderObject);
-    gl.attachShader(shaderProgram, fragmentShaderObject);
-    gl.linkProgram(shaderProgram);
-    gl.useProgram(shaderProgram);
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentShaderCode);
+  gl.compileShader(fragmentShader);
 
-    gl.clearColor(0.0,0.0,0.0,0.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
+
+  // Set warna background
+  gl.clearColor(0.9, 0.2, 0.0, 0.5);
+  // Bersihkan buffer warna
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Gambar titik
+  gl.drawArrays(gl.POINTS, 0, 1);
 }
