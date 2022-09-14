3432409aaa18d5772215b86b32156c9a103f2aae
Trelel129 <ferrynur77@gmail.com>
2022-09-14 15:11:44 +0700

    3 dots

--- main.js
+++ main.js
@@ -3,14 +3,25 @@ function main() {
   var canvas = document.getElementById("myCanvas");
   // Ambil konteks WebGL
   var gl = canvas.getContext("webgl");
+  //array 1 dimensi
+var vertices = [
+  0.5, 0.5, 
+  0.0, 0.0, 
+  -0.5, 0.5
+];
+
+var buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
   //posisi
   var vertexShaderCode = `
+  attribute vec2 aPosition;
     void main() {
-      float x = 0.0;
-      float y = 0.0;
+      float x = aPosition.x;
+      float y = aPosition.y;
       gl_PointSize = 10.0;
-      gl_Position = vec4(x, y, 0.0, 1.0);
+      gl_Position = vec4(aPosition.xy, 0.0, 1.0);
     }
   `;
 
@@ -39,11 +50,18 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
+  //Kita mengajari GPU bagaimana caranya mengoleksi
+  // nilai posisi dari ARRAY_BUFFER
+  // untuk setiap vertex yang sedang diproses
+  var aPosition = gl.getAttribLocation(shaderProgram,"aPosition");
+gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(aPosition);
+
   // Set warna background
-  gl.clearColor(0.9, 0.2, 0.0, 0.5);
+  gl.clearColor(0.9, 0.65, 0.0, 0.5);
   // Bersihkan buffer warna
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Gambar titik
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.drawArrays(gl.POINTS, 0, 3);
 }
