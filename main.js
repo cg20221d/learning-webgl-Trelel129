6a213af290f049efc6d4646f7f43210c9793ecc9
Trelel129 <ferrynur77@gmail.com>
2022-10-12 14:15:50 +0700

    translasi matriks dalam shader

--- main.js
+++ main.js
@@ -20,9 +20,16 @@ function main() {
   uniform float uTheta;
   varying vec3 vColor;
   void main() {
-      float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
-      float y = cos(uTheta) * aPosition.x + sin(uTheta) * aPosition.y;
-      gl_Position = vec4(x, y, 0.0, 1.0);
+      // float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
+      // float y = cos(uTheta) * aPosition.x + sin(uTheta) * aPosition.y;
+      // gl_Position = vec4(x, y, 0.0, 1.0);
+      vec2 position = aPosition;
+      vec3 d = vec3(0.5, -0.5, 0.0);
+      mat4 translation = mat4(1.0, 0.0, 0.0, 0.0,
+                            0.0, 1.0, 0.0, 0.0,
+                            0.0, 0.0, 1.0, 0.0,
+                            d.x, d.y, d.z, 1.0);      
+      gl_Position = translation* vec4(position,0.0,1.0);
       vColor = aColor;
   }
   `;
